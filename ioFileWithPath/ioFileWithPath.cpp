c1a3368d68ddfe9621fb09b7ef8993cc02fcaa63
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-06-19 15:09:20 +0700

    membuat tujuan file dengan inputan nama file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -10,5 +10,12 @@ int main() {
 	cout << "Masukkan Nama File : ";
 	cout << namafile;
 
+	// membuka file dan menulis.
+	ofstream outfile;
+	// menunjuk ke sebuah nama file
+	outfile.open(namafile + ".txt", ios::out);
 
+	cout << ">= Menulis file, \ ketik 'q\' untuk Keluar" << endl;
+
+	//
 }
