a75e8f81f8e7bd5fc5b73cf238fe74c127099622
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-06-19 15:06:59 +0700

    Mmebuat variabel dan inputan namafile

---
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -0,0 +1,14 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string baris;
+	string namafile;
+
+	cout << "Masukkan Nama File : ";
+	cout << namafile;
+
+
+}
